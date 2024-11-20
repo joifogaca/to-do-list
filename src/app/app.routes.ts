b1f75022a2c406ccd1c81eaf2bd32a95d5e4faed
Joice Fogaça <joi.fogaca@gmail.com>
2024-11-20 12:42:48 -0300

    feat(): Profile module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile-module.module').then(m => m.ProfileModuleModule),
+    data: { preload: true }
+  },
+];
---
+++ src/app/profile/profile-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileModuleComponent } from './profile-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProfileModuleComponent]
+})
+export class ProfileModuleModule { }
