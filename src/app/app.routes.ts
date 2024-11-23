d4fc1e6678c0faf70dee76a44b73da89a095cfb9
Joice Fogaça <joi.fogaca@gmail.com>
2024-11-23 12:10:34 -0300

    conf(): add bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.component').then(m => m.ProfileComponent),
+    path: '',
+    loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
     data: { preload: true }
   },
 ];
