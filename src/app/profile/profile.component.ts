f93b2fc7071543cbb35558b29233c832a8dfcce0
Joice Fogaça <joi.fogaca@gmail.com>
2024-12-02 13:01:20 -0300

    feat(): novos campos

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from './foto/foto.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FotoComponent],
+  imports: [FotoComponent, FormsModule],
+  providers: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+
+  nome: string ='';
+  email: string ='joi.fogaca@gmail.com';
 
 }
