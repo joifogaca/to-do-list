a583e84e92bdf6be70222088be281d5daacd865d
Joice Fogaça <joi.fogaca@gmail.com>
2024-12-07 17:14:49 -0300

    tirado ngModel

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,25 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FotoComponent } from './foto/foto.component';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [FotoComponent, FormsModule],
-  providers: [],
+  providers: [Router],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
-  constructor(private router: Router) {}
-
-  goToAlterarEmail(): void {
-    this.router.navigate(['profile/altera-email']);
-  }
-
   nome: string ='';
   email: string ='joi.fogaca@gmail.com';
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  goToAlterarEmail(): void {
+    console.log('goToAlterarEmail');
+    this.router.navigate(['altera-email'], {relativeTo: this.route});
+  }
+
+
 
 }
