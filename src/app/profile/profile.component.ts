963b6e05c41557498fa14ba9eda1c03b66c0c648
Joice Fogaça <joi.fogaca@gmail.com>
2024-12-07 17:14:13 -0300

    corrigido rotas

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from './foto/foto.component';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +12,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+
+  constructor(private router: Router) {}
+
+  goToAlterarEmail(): void {
+    this.router.navigate(['profile/altera-email']);
+  }
 
   nome: string ='';
   email: string ='joi.fogaca@gmail.com';
