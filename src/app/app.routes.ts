e70ec896f0ccb4f8949402d165e692381056fac2
Joice Fogaça <joi.fogaca@gmail.com>
2024-12-07 17:15:07 -0300

    corrigida rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
-    data: { preload: true }
-  },
+    data: { preload: true },
+  },{
+        path: 'altera-email',
+        loadComponent: () => import('./profile/altera-email/altera-email.component').then(m => m.AlteraEmailComponent),
+      }
 ];
